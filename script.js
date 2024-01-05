cecbab07dd87c79aa5760855f8d45bfbff121095
Md Azmi Siddique <md.azmi.siddique@gmail.com>
2024-01-05 08:51:11 +0600

    age is presenting

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-let button = document.getElementById("submit");
+let inputAge = document.getElementById("inputAge");
+var showAge = document.getElementById('showAge');
+var showImg = document.getElementById('showImg');
+var showTitle = document.getElementById('showTitle');
 
-button.addEventListener("click", () => {
-  let age = document.getElementById("age"); 
-  let show = document.getElementById("show");
 
-  let ageGrab = age.value;
+inputAge.addEventListener("input", () => {
+  var age = parseFloat(inputAge.value);
 
-  let parsedAge = parseInt(ageGrab);
+  if (age >= 18) {
+    showAge.style.display = 'block';
+    showImg.src = './assets/adult.jpg';
+    showTitle.innerText = "You are an adult";
 
-  if (parsedAge >= 18) {
-    show.innerText = "You are an adult";
-  }
-  else if(parsedAge<18){
-    show.innerText = "You are a child";
-  }
-  else{
-    show.innerText = "please enter a valid number"
+    // showAge.innerText = "You are an adult";
+    console.log("adult");
+  } else{
+    showAge.style.display = 'block';
+    showImg.src = './assets/child.jpeg';
+    showTitle.innerText = "You are an child";
   }
 });
